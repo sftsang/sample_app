ec68abd0524f56dba79d0118fc59fcf003af6101
Sherman Tsang <sherman@e19.ca>
2011-03-26 16:49:42 -0600

    microposts exercises

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,11 @@
 class MicropostsController < ApplicationController
   before_filter :authenticate, :only => [:create, :destroy]
   before_filter :authorized_user, :only => :destroy
+  
+  def index
+    @micropost = Micropost.new
+    @feed_items = current_user.feed.paginate(:page => params[:page])
+  end
 
   def create
     @micropost  = current_user.microposts.build(params[:micropost])
